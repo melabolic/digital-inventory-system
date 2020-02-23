cac382a285e97e217ea3f18b86fa83672d042af8
melabolic <jiewen.lim@minerva.kgi.edu>
2020-02-23 13:50:26 +0800

    Added data validation to input fields

--- lib/screens/input_fields.dart
+++ lib/screens/input_fields.dart
@@ -16,6 +16,9 @@ class InputFields extends StatefulWidget {
 }
 
 class _InputFieldsState extends State<InputFields> {
+  // here, we're creating a global key to keep track and validate our user inputs
+  var _formKey = GlobalKey<FormState>();
+
   DatabaseHelper databaseHelper = DatabaseHelper();
   Item item;
   String appBarTitle;
@@ -33,124 +36,152 @@ class _InputFieldsState extends State<InputFields> {
     weightController.text = item.weight;
     dateController.text = item.expiryDate;
 
+    // the widget WillPopScope enables us to control the leading buttons actions on the appbar
     return WillPopScope(
       onWillPop: () {
-        moveToLastScreen();
+        moveToLastScreen(); // takes us back to the last screen
         return;
       },
       child: Scaffold(
         appBar: AppBar(
           leading: IconButton(
             icon: Icon(Icons.arrow_back),
-            onPressed: () => moveToLastScreen(),
+            onPressed: () =>
+                moveToLastScreen(), // takes us back to the last screen
           ),
           title: Text(appBarTitle),
         ),
-        body: _mainBody(),
+        body: _mainBody(), // main code that displays the input fields
       ),
     );
   }
 
-  // creating the widget that displays the text editors
+  // creating the widget that displays the input fields
   Widget _mainBody() {
-    return Padding(
-      padding: EdgeInsets.only(top: 12.0, left: 24.0, right: 24.0),
-      child: ListView(
-        children: <Widget>[
-          // updating the name entry
-          Padding(
-            padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-            child: TextField(
-              textCapitalization: TextCapitalization.words,
-              controller: nameController,
-              keyboardType: TextInputType.text,
-              onChanged: (value) {
-                debugPrint(value);
-                updateName();
-              },
-              decoration: InputDecoration(
-                  labelText: 'Item Name',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0))),
-            ),
-          ),
-          // updating the expiry date
-          Padding(
+    return Form(
+      key: _formKey, // using this key to get the current instance of our form
+      child: Padding(
+        padding: EdgeInsets.only(top: 12.0, left: 24.0, right: 24.0),
+        child: ListView(
+          children: <Widget>[
+            // updating the name entry
+            Padding(
               padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-              child: TextField(
-                controller: dateController,
-                keyboardType: TextInputType.number,
+              child: TextFormField(
+                textCapitalization: TextCapitalization.words,
+                controller: nameController,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'Please enter a name';
+                  } else if (value.length < 3) {
+                    return 'Item name is too short';
+                  }
+                },
+                keyboardType: TextInputType.text,
                 onChanged: (value) {
                   debugPrint(value);
-                  updateDate();
+                  updateName();
                 },
                 decoration: InputDecoration(
-                    hintText: "DD/MM/YYYY",
-                    labelText: 'Expiry Date',
+                    labelText: 'Item Name',
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10.0))),
-              )),
-          // updating the weight entry
-          Padding(
-            padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-            child: TextField(
-              controller: weightController,
-              keyboardType: TextInputType.numberWithOptions(),
-              onChanged: (value) {
-                debugPrint(value);
-                updateWeight();
-              },
-              decoration: InputDecoration(
-                  labelText: 'Weight (in grams)',
-                  hintText: '1kg = 1000g',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0))),
+              ),
             ),
-          ),
-          // Save and Delete Buttons
-          Padding(
-            padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: RaisedButton(
-                    color: Theme.of(context).primaryColorLight,
-                    textColor: Theme.of(context).primaryColorDark,
-                    child: Text(
-                      'Save',
-                      textScaleFactor: 1.5,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        debugPrint("Save button clicked");
-                        _save();
-                      });
-                    },
-                  ),
-                ),
-                Container(
-                  width: 15.0,
-                ),
-                Expanded(
-                  child: RaisedButton(
-                    color: Theme.of(context).primaryColorLight,
-                    textColor: Theme.of(context).primaryColorDark,
-                    child: Text(
-                      (item.id != null) ? 'Delete' : 'Cancel',
-                      textScaleFactor: 1.5,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        debugPrint("Delete button clicked");
-                        _delete();
-                      });
-                    },
-                  ),
-                ),
-              ],
+            // updating the expiry date
+            Padding(
+                padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                child: TextFormField(
+                  controller: dateController,
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return 'Expiry date is required';
+                    } else if (value.length < 10) {
+                      return 'Date is not valid';
+                    }
+                  },
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    debugPrint(value);
+                    updateDate();
+                  },
+                  decoration: InputDecoration(
+                      hintText: "DD/MM/YYYY",
+                      labelText: 'Expiry Date',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                )),
+            // updating the weight entry
+            Padding(
+              padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+              child: TextFormField(
+                controller: weightController,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'Please enter the weight';
+                  }
+                },
+                keyboardType: TextInputType.numberWithOptions(),
+                onChanged: (value) {
+                  debugPrint(value);
+                  updateWeight();
+                },
+                decoration: InputDecoration(
+                    labelText: 'Weight (in grams)',
+                    hintText: '1kg = 1000g',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+              ),
             ),
-          ),
-        ],
+            // Save and Delete Buttons
+            Padding(
+              padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: RaisedButton(
+                      color: Theme.of(context).primaryColorLight,
+                      textColor: Theme.of(context).primaryColorDark,
+                      child: Text(
+                        'Save',
+                        textScaleFactor: 1.5,
+                      ),
+                      onPressed: () {
+                        setState(
+                          () {
+                            if (_formKey.currentState.validate()) {
+                              debugPrint("Save button clicked");
+                              _save();
+                            }
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    width: 15.0,
+                  ),
+                  Expanded(
+                    child: RaisedButton(
+                      color: Theme.of(context).primaryColorLight,
+                      textColor: Theme.of(context).primaryColorDark,
+                      child: Text(
+                        (item.id != null) ? 'Delete' : 'Cancel',
+                        textScaleFactor: 1.5,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          debugPrint("Delete button clicked");
+                          _delete();
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
